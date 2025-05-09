8a94ecabebd057c1400d2dcbaa92bd21d3d1d43d
Vitalii Vasylets <mohara88s@gmail.com>
2025-05-09 08:00:28 +0300

    add comments to task3

--- task3.py
+++ task3.py
@@ -5,11 +5,14 @@ brackets_dict = {'{':'}','[':']','(':')'}
 
 # Функція перевірки на симетричність дужок
 def is_symmetry_of_brackets(text):
+    # Створюю стек
     stack = []
+    # Проходжусь по тексту
     for s in text:
-        print(s)
+        # За умови що дужка відкриваюча додаю на стек
         if s in brackets_dict.keys():
             stack.append(s)
+        # За умови що дужка закриваюча перевіряю чи є відповідна відкриваюча останньою в стеку, та зараховую пару, видаленням відкриваючої дужки зі стеку. Якщо пари нема то помилка.
         if s in brackets_dict.values():
             if len(stack) == 0:
                 print("Error with brackets! You should open then close!")
@@ -19,21 +22,28 @@ def is_symmetry_of_brackets(text):
             else:
                 print("Error with symmetry of brackets!")
             
-        
-        print(stack)
-
+    # Яккщо лишилися відкриті дужки то помилка
     if len(stack) != 0:
         print("Error with the number of brackets!")
         return False
-    
+    # Якщо дійшли сюди то ми молодці, бо магія дужок виконалася і прекрасна симетрія підтверджена!
+    # :)
+    # Гарного дня!
     return True
 
 
-        
 
 def main():
-    string = "І (що сало? Ласощі…)"
-    is_symmetry_of_brackets(string)
+    # Перевірки
+    assert is_symmetry_of_brackets("Дід( і )дід") is True
+    assert is_symmetry_of_brackets("Дід( і дід") is False
+    assert is_symmetry_of_brackets("Дід( {і} )дід") is True
+    assert is_symmetry_of_brackets("Дід{(} і )дід") is False
+
+    # Перевірки згідно прикладу
+    assert is_symmetry_of_brackets("( ){[ 1 ]( 1 + 3 )( ){ }}:") is True
+    assert is_symmetry_of_brackets("( 23 ( 2 - 3);:") is False
+    assert is_symmetry_of_brackets("( 11 }:") is False
 
 if __name__=="__main__":
     main()
